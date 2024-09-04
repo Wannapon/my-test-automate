aa081f721f9f3bdf761bc0562e51469b010f8475
Wannapon <wannnapon@builk.com>
2024-09-05 02:35:22 +0700

    fixed error in it loop

--- cypress/e2e/Scenarios/employee-loop.cy.js
+++ cypress/e2e/Scenarios/employee-loop.cy.js
@@ -2,16 +2,16 @@ describe('loop employee id', () => {
 
     let employee_id = ['user1', 'user2', 'user3', 'user4', 'user5']
 
-    it('test loop', () => {
-        for (let i = 0; i < employee_id.length; i++) {
-            const emp_id = employee_id[i];
-            it('scenario ' + i, () => {
-                cy.log('i = ' + i)
-                cy.log('empid = ' + employee_id)
-                cy.log('Employee id = ' + emp_id)
-            });
-        }
-    });
-    // lolololololololololol //
+    //start loop
+    for (let i = 0; i < employee_id.length; i++) {
+        const emp_id = employee_id[i];
+
+        // start it //
+        it('scenario ' + i, () => {
+            cy.log('i = ' + i)
+            cy.log('empid = ' + employee_id)
+            cy.log('Employee id = ' + emp_id)
+        });
+    }
 
 });
